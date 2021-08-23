6fda78375fc39c1adb4c375ca76eaf9265222235
Katherine-valcarce <katherine.v@live.cl>
2021-08-23 19:16:01 -0400

    devuelve listado de archivos e identifica si es directorio

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const route = __dirname;
+const path = require("path");
+
+fs.readdir(route, 'utf8', (err, list) => {
+
+  list.forEach(list => {
+      let mdFile = path.extname(list);
+      const isDirectory = fs.lstatSync(list).isDirectory() 
+      console.log(list, isDirectory)
+      if (mdFile == '.md') { 
+          mdFile.includes(list);
+          console.log(list, isDirectory);
+      }
+  })
+})
+
+/*fs.readdir(route,(error, files)=> {
+    if(error){
+        throw error;
+      }
+      files.forEach(element => {
+        const isDirectory = fs.lstatSync().isDirectory() 
+        console.log(isDirectory)
+      });
+     console.log(files)
+})*/
---
+++ readmeTest.md
@@ -0,0 +1,8 @@
+LINKS BUENOS
+[Link a Google](https://google.com/)
+[Link de Facebook](https://facebook.com/)
+[Link de gatitos](https://sp.depositphotos.com/stock-photos/gatito.html)
+
+LINKS MALOS
+[Link malo 1](https://linkmalomalditoxd.com/)
+[Link malo 2](https://esteLinksEstaMalo.cl/)
