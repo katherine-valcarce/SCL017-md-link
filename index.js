10273551d93154c746cd0cb2202c3a28e14469ac
Katherine-valcarce <katherine.v@live.cl>
2021-08-25 18:29:22 -0400

    toma los archivos MD y los leé

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const path = require("path");
 module.exports = (dirPath) => {
   return readMyFile(dirPath);
 }
+let listMD = [];
 
 const readMyFile = (dirPath) => {
   return new Promise ((resolve, reject) => {
-    fs.readFile(dirPath, 'utf8', (err, data) => {
+    fs.readdir(dirPath, 'utf8', (err, list) => {
+      list.forEach(list => {
+        let mdFile = path.extname(list);
+        if (mdFile == '.md') { 
+          listMD.push(list)
+            for (let i = 0; i < listMD.length; i++) {
+              fs.readFile((listMD[i].toString()), 'utf8', (err, data) => {
+                if (err) reject(err);
+                resolve(data)
+              })
+            } 
+        }
+    })
+    })
+    /*fs.readFile(listMD, 'utf8', (err, data) => {
       if (err) reject(err);
       resolve(data)
-    })
+    })*/
   })
 }
 /* module.exports = () => {
