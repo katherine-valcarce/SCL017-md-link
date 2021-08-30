8001bed56d35eafd669e266bcd0dcd5b2462785e
Katherine-valcarce <katherine.v@live.cl>
2021-08-30 08:32:58 -0400

    lee archivos md - falta aplicar recursividad

--- cli.js
+++ cli.js
@@ -2,5 +2,5 @@ const mdLinks = require('./index.js');
 const path = process.argv[2];
 
 mdLinks(path)
-.then(file => console.log(file))
+.then((file) => console.log(file))
 .catch(err => console.log('Error al ejecutar', err));
---
+++ pruebas.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = process.argv[2];
+
+
+fs.readdir(path, 'utf8', (err, list) => {
+    list.forEach(list => {
+        let listMD = [];
+        const isDirectory = fs.lstatSync(list).isDirectory();
+        if( isDirectory == true ) {
+            const arrayList = listMD.includes(list);
+            if (arrayList==false) {
+                listMD.push(list);
+            }
+            for (let i = 0; i < listMD.length; i++) {
+            return fs.readdir((listMD[i].toString()), 'utf8', (err, list) => {
+                list.forEach(list => {
+                   
+                    console.log(list);
+                })
+            })
+          }
+        } 
+    return list; 
+    })
+})
